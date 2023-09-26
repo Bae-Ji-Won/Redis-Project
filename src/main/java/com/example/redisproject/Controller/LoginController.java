6d989762c6f39913d78aac292748bf2f421fa978
Bae Ji Won <qowhxk@naver.com>
2023-09-26 18:57:41 +0900

    Login Session - Redis 사용

---
+++ src/main/java/com/example/redisproject/Controller/LoginController.java
@@ -0,0 +1,29 @@
+package com.example.redisproject.Controller;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+@RestController
+public class LoginController {
+
+    
+    // redis를 활용하여 로그인한 데이터 저장
+
+    @GetMapping("/login")
+    public String Login(HttpSession session, @RequestParam String name){
+        session.setAttribute("name",name);   // 세션에 로그인한 이름 저장
+
+        return "saved";
+    }
+
+    @GetMapping("/myname")
+    public String MyName(HttpSession session){
+        String myName = (String)session.getAttribute("name");   // 세션에 저장된 이름 호출
+
+        return myName;
+    }
+}
