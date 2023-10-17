31817802ce23c72bdc0732cf85aeb50e3f24662d
Bae Ji Won <qowhxk@naver.com>
2023-10-17 23:03:59 +0900

    Redis를 활용하여 캐싱 설정 파일

---
+++ src/main/java/com/example/redisproject/config/RedisCacheConfig.java
@@ -0,0 +1,39 @@
+package com.example.redisproject.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.CacheKeyPrefix;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+import java.util.HashMap;
+
+@Configuration
+// Redis 캐싱 설정 파일
+public class RedisCacheConfig {
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        RedisCacheConfiguration configuration = RedisCacheConfiguration.defaultCacheConfig()
+                .disableCachingNullValues()
+                .entryTtl(Duration.ofSeconds(10))   // 기본 TTL
+                .computePrefixWith(CacheKeyPrefix.simple())
+                .serializeKeysWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer())
+                );
+
+        HashMap<String, RedisCacheConfiguration> configMap = new HashMap<>();
+        configMap.put("userAgeCache", RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofSeconds(5)));  // 특정 캐시에 대한 TTL   (5초만 Redis에 데이터 저장시킴)
+
+        return RedisCacheManager
+                .RedisCacheManagerBuilder
+                .fromConnectionFactory(connectionFactory)
+                .cacheDefaults(configuration)
+                .withInitialCacheConfigurations(configMap)
+                .build();
+    }
+}
